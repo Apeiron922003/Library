c317daf44b2c2784f50cc5400e89ad33f66de7c7
Vu Duc Tai <apeironisme@gmail.com>
2024-06-14 18:17:48 +0700

    second

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,33 +2,19 @@ require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const booksRouter = require("./routes/books");
 
 const apiRouter = require("./routes/api/api");
 const app = express();
 app.use(cors());
-// view engine setup
-const expressLayouts = require("express-ejs-layouts");
+
 const responseHelper = require("./helper/response.helper");
-app.use(expressLayouts);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, "../public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/books", booksRouter);
 
 app.use("/api", apiRouter);
 
--- backend/src/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-const userController = require("../controllers/clients/user.controller");
-var router = express.Router();
-
-/* GET users listing. */
-
-router.get("/", userController.index);
-router.get("/:id", userController.detail);
-module.exports = router;
